a32197cea0d1f5e65283f6b5b58dc12dafb307a9
Hiligen723 <gen.li@tum.de>
2026-01-08 12:32:27 +0100

    rename folder to `src`

--- py_script/submission/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Submission utilities for Huawei TechArena 2025 Phase 2
-
-This module provides utilities for converting MPC optimization results
-to the required submission format (Excel files with multiple sheets).
-"""
-
-__all__ = ['convert_results', 'format_outputs']
