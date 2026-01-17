5654a988054757b7566a3ea38f271e1920c7a4fb
Hiligen723 <gen.li@tum.de>
2026-01-17 13:25:01 +0100

    introduce `config_loader.py` to fit `config.yml`; update `requirements.txt` accordingly

---
+++ src/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Utils module for GridKey BESS Optimizer.
+
+Provides centralized utilities including configuration loading.
+"""
+
+from .config_loader import ConfigLoader
+
+__all__ = ['ConfigLoader']
